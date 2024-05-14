374894a68deade47c20c6dbb2f42260e15339018
António Miller <up202008423@up.pt>
2024-05-14 17:00:25 +0100

    Primeira implementação

--- readSVG.cpp
+++ readSVG.cpp
@@ -9,6 +9,133 @@ using namespace tinyxml2;
 
 namespace svg
 {
+    XMLElement* parseGroup(XMLElement* child, vector<SVGElement *>& svg_elements){
+
+        while (child != nullptr){
+            std::string elementName = child->Name();
+            SVGElement* element;
+
+            if (elementName == "ellipse"){
+                    int cx = child->IntAttribute("cx");
+                    int cy = child->IntAttribute("cy");
+                    int rx = child->IntAttribute("rx");
+                    int ry = child->IntAttribute("ry"); 
+                    string fill = child->Attribute("fill");
+                    Color fillColor = parse_color(fill);
+
+                    Point center = Point{cx,cy};
+                    element = new Ellipse(fillColor, center, rx, ry); 
+            }
+            else if (elementName == "circle"){
+                int cx = child->IntAttribute("cx");
+                int cy = child->IntAttribute("cy");
+                int r = child->IntAttribute("r");
+                string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
+
+                Point center = Point{cx,cy};
+
+                element = new Circle(fillColor, center, r);
+            }
+            else if (elementName == "polyline") {
+                    std::string pointsStr = child->Attribute("points");
+                    std::vector<Point> points = parse_points(pointsStr);
+                    std::string stroke = child->Attribute("stroke");
+                    Color strokeColor = parse_color(stroke);
+
+                    element = new Polyline(strokeColor, points);
+            }
+            else if (elementName == "line") {
+                    int x1 = child->IntAttribute("x1");
+                    int y1 = child->IntAttribute("y1");
+                    int x2 = child->IntAttribute("x2");
+                    int y2 = child->IntAttribute("y2");
+                    std::string stroke = child->Attribute("stroke");
+                    Color strokeColor = parse_color(stroke);
+
+                    element = new Line(strokeColor, x1,y1,x2,y2);
+            }
+            else if (elementName == "polygon") {
+                std::string pointsStr = child->Attribute("points");
+                std::vector<Point> points = parse_points(pointsStr);
+                std::string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
+
+                element = new Polygon(fillColor, points);
+            }
+            else if (elementName == "rect") {
+                int x = child->IntAttribute("x");
+                int y = child->IntAttribute("y");
+                int width = child->IntAttribute("width");
+                int height = child->IntAttribute("height");
+                std::string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
+
+                element = new Rect(fillColor, x, y, width, height);
+            } 
+            else if(elementName == "g") {
+                parseGroup(child->FirstChildElement(), svg_elements);
+            }
+
+            //transf
+            if (child->Attribute("transform")){
+                        //!@param tr Transformação do elemento
+                        string tr = child->Attribute("transform");
+                        
+                        int ox = 0;
+                        int oy = 0; //default origin values
+
+                        Point origin = Point{ox, oy};
+
+                        if (child->Attribute("transform-origin")){
+                            string origin_str = child->Attribute("transform-origin");
+                            istringstream iss(origin_str);
+                            int x, y;
+                            iss >> x >> y;
+
+                            origin.x = x;
+                            origin.y = y;
+                        }
+
+                        if(tr.find("translate")!= string::npos){
+                            istringstream iss(tr);
+
+                            iss.ignore(tr.size(), '(');
+                            int x, y;
+                            iss >> x >> y;
+
+                            element->translate(x, y);
+                        }
+                        else if(tr.find("rotate") != string::npos){
+                            istringstream iss(tr);
+
+                            iss.ignore(tr.size(), '(');
+                            int angle;
+                            iss >> angle;
+
+                            
+
+                            element->rotate(origin.x, origin.y, angle);
+                        }
+                        else if(tr.find("scale") != string::npos){
+                            istringstream iss(tr);
+
+                            iss.ignore(tr.size(), '(');
+                            int factor;
+                            iss >> factor;
+
+                            element->scale(origin.x, origin.y, factor);
+                        }
+                    }
+
+            //push back
+            svg_elements.push_back(element);
+            child = child->NextSiblingElement();
+        }
+        return child;
+    }
+
+
     void readSVG(const string& svg_file, Point& dimensions, vector<SVGElement *>& svg_elements)
     {
         XMLDocument doc;
@@ -87,9 +214,16 @@ namespace svg
 
                 element = new Rect(fillColor, x, y, width, height);
             } 
-
+            else if(elementName == "g") {
+                child = parseGroup(child->FirstChildElement(),  svg_elements);
+                cout << "Here " << endl;
+            }
+            if(child == nullptr){
+                
+                return;
+            } 
             //transf
-             if (child->Attribute("transform")){
+            if (child->Attribute("transform")){
                         //!@param tr Transformação do elemento
                         string tr = child->Attribute("transform");
                         
