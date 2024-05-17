6d681d96df8371df64dded130a6ee1767f79367c
diogo <diogopmac@diogopmac.local>
2024-05-17 10:24:38 +0100

    Some comments done

--- readSVG.cpp
+++ readSVG.cpp
@@ -9,22 +9,28 @@ using namespace tinyxml2;
 
 namespace svg
 {
+    /// @brief Function to treat groups. 
+    /// @param child XML node representing the group
+    /// @param elements Empty vector of SVGElement objects
+    /// @param id_pair Vector of pair SVGElement and ID, for use type usage
+    /// @return Vector of SVGElement, with all the elements of the group represented in the node
     std::vector<SVGElement *> parseGroup(XMLElement* child, vector<SVGElement *>& elements, vector<pair<std::string, SVGElement *>>& id_pair){
 
+        // Same logic as ReadSVG
         while (child != nullptr){
             std::string elementName = child->Name();
             SVGElement* element;
 
             if (elementName == "ellipse"){
-                    int cx = child->IntAttribute("cx");
-                    int cy = child->IntAttribute("cy");
-                    int rx = child->IntAttribute("rx");
-                    int ry = child->IntAttribute("ry"); 
-                    string fill = child->Attribute("fill");
-                    Color fillColor = parse_color(fill);
+                int cx = child->IntAttribute("cx");
+                int cy = child->IntAttribute("cy");
+                int rx = child->IntAttribute("rx");
+                int ry = child->IntAttribute("ry"); 
+                string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
 
-                    Point center = Point{cx,cy};
-                    element = new Ellipse(fillColor, center, rx, ry); 
+                Point center = Point{cx,cy};
+                element = new Ellipse(fillColor, center, rx, ry); 
             }
             else if (elementName == "circle"){
                 int cx = child->IntAttribute("cx");
@@ -34,26 +40,25 @@ namespace svg
                 Color fillColor = parse_color(fill);
 
                 Point center = Point{cx,cy};
-
                 element = new Circle(fillColor, center, r);
             }
             else if (elementName == "polyline") {
-                    std::string pointsStr = child->Attribute("points");
-                    std::vector<Point> points = parse_points(pointsStr);
-                    std::string stroke = child->Attribute("stroke");
-                    Color strokeColor = parse_color(stroke);
+                std::string pointsStr = child->Attribute("points");
+                std::vector<Point> points = parse_points(pointsStr);
+                std::string stroke = child->Attribute("stroke");
+                Color strokeColor = parse_color(stroke);
 
-                    element = new Polyline(strokeColor, points);
+                element = new Polyline(strokeColor, points);
             }
             else if (elementName == "line") {
-                    int x1 = child->IntAttribute("x1");
-                    int y1 = child->IntAttribute("y1");
-                    int x2 = child->IntAttribute("x2");
-                    int y2 = child->IntAttribute("y2");
-                    std::string stroke = child->Attribute("stroke");
-                    Color strokeColor = parse_color(stroke);
+                int x1 = child->IntAttribute("x1");
+                int y1 = child->IntAttribute("y1");
+                int x2 = child->IntAttribute("x2");
+                int y2 = child->IntAttribute("y2");
+                std::string stroke = child->Attribute("stroke");
+                Color strokeColor = parse_color(stroke);
 
-                    element = new Line(strokeColor, x1,y1,x2,y2);
+                element = new Line(strokeColor, x1,y1,x2,y2);
             }
             else if (elementName == "polygon") {
                 std::string pointsStr = child->Attribute("points");
@@ -83,75 +88,76 @@ namespace svg
                 href = href.substr(1);
                 for(auto& pair : id_pair)
                 {
-                if (pair.first == href)
+                    if (pair.first == href)
                     {
-                    element = pair.second->duplicate(href, pair.second);
+                        element = pair.second->duplicate(href, pair.second);
                     }
                 }
             }
 
             //transf
             if (child->Attribute("transform")){
-                        //!@param tr Transformação do elemento
-                        string tr = child->Attribute("transform");
-                        
-                        int ox = 0;
-                        int oy = 0; //default origin values
+                //!@param tr Transformação do elemento
+                string tr = child->Attribute("transform");
+                
+                int ox = 0;
+                int oy = 0; //default origin values
 
-                        Point origin = Point{ox, oy};
+                Point origin = Point{ox, oy};
 
-                        if (child->Attribute("transform-origin")){
-                            string origin_str = child->Attribute("transform-origin");
-                            istringstream iss(origin_str);
-                            int x, y;
-                            iss >> x >> y;
+                if (child->Attribute("transform-origin")){
+                    string origin_str = child->Attribute("transform-origin");
 
-                            origin.x = x;
-                            origin.y = y;
-                        }
+                    istringstream iss(origin_str);
+                    int x, y;
+                    iss >> x >> y;
 
-                        if(tr.find("translate")!= string::npos){
-                            istringstream iss(tr);
+                    origin.x = x;
+                    origin.y = y;
+                }
 
-                            int x, y;
-                            char ch;
+                if(tr.find("translate")!= string::npos){
+                    istringstream iss(tr);
 
-                            iss.ignore(tr.size(), '(');
-                            iss >> x;
-                            if (iss.peek() == ',') iss >> ch;
-                            iss  >> y;
+                    int x, y;
+                    char ch;
 
-                            element->translate(x, y);
-                        }
-                        else if(tr.find("rotate") != string::npos){
-                            istringstream iss(tr);
+                    iss.ignore(tr.size(), '(');
+                    iss >> x;
+                    if (iss.peek() == ',') iss >> ch;
+                    iss  >> y;
 
-                            iss.ignore(tr.size(), '(');
-                            int angle;
-                            iss >> angle;
+                    element->translate(x, y);
+                }
+                else if(tr.find("rotate") != string::npos){
+                    istringstream iss(tr);
 
-                            
+                    iss.ignore(tr.size(), '(');
+                    int angle;
+                    iss >> angle;
 
-                            element->rotate(origin.x, origin.y, angle);
-                        }
-                        else if(tr.find("scale") != string::npos){
-                            istringstream iss(tr);
+                    element->rotate(origin.x, origin.y, angle);
+                }
+                else if(tr.find("scale") != string::npos){
+                    istringstream iss(tr);
 
-                            iss.ignore(tr.size(), '(');
-                            int factor;
-                            iss >> factor;
+                    iss.ignore(tr.size(), '(');
+                    int factor;
+                    iss >> factor;
 
-                            element->scale(origin.x, origin.y, factor);
-                        }
+                    element->scale(origin.x, origin.y, factor);
+                }
             }
             //push back
             if (child->Attribute("id")){
-            std::string id = child->Attribute("id");
-            id_pair.push_back({id, element});
+                std::string id = child->Attribute("id");
+                id_pair.push_back({id, element});
             }
+
             elements.push_back(element);
             child = child->NextSiblingElement();
         }
+        // Returns the vector of SVGElement, later to create the object Group
         return elements;
     }
 
@@ -174,137 +180,140 @@ namespace svg
         XMLElement *child = xml_elem->FirstChildElement(); //Changed from svg_elem
 
         while (child != nullptr){
-
+            
+            //!@param elementName Name of the current element considered
+            //!@param element Object to be created 
             std::string elementName = child->Name();
             SVGElement* element;
 
             if (elementName == "ellipse"){
-                    int cx = child->IntAttribute("cx");
-                    int cy = child->IntAttribute("cy");
-                    int rx = child->IntAttribute("rx");
-                    int ry = child->IntAttribute("ry"); 
-                    string fill = child->Attribute("fill");
-                    Color fillColor = parse_color(fill);
+                int cx = child->IntAttribute("cx");
+                int cy = child->IntAttribute("cy");
+                int rx = child->IntAttribute("rx");
+                int ry = child->IntAttribute("ry"); 
+                string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
 
-                    Point center = Point{cx,cy};
-                    element = new Ellipse(fillColor, center, rx, ry); 
+                Point center = Point{cx,cy};
+                element = new Ellipse(fillColor, center, rx, ry); 
             }
             else if (elementName == "circle"){
-                    int cx = child->IntAttribute("cx");
-                    int cy = child->IntAttribute("cy");
-                    int r = child->IntAttribute("r");
-                    string fill = child->Attribute("fill");
-                    Color fillColor = parse_color(fill);
+                int cx = child->IntAttribute("cx");
+                int cy = child->IntAttribute("cy");
+                int r = child->IntAttribute("r");
+                string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
 
-                    Point center = Point{cx,cy};
-
-                    element = new Circle(fillColor, center, r);
+                Point center = Point{cx,cy};
+                element = new Circle(fillColor, center, r);
             }
             else if (elementName == "polyline") {
-                    std::string pointsStr = child->Attribute("points");
-                    std::vector<Point> points = parse_points(pointsStr);
-                    std::string stroke = child->Attribute("stroke");
-                    Color strokeColor = parse_color(stroke);
+                std::string pointsStr = child->Attribute("points");
+                std::vector<Point> points = parse_points(pointsStr);
+                std::string stroke = child->Attribute("stroke");
+                Color strokeColor = parse_color(stroke);
 
-                    element = new Polyline(strokeColor, points);
+                element = new Polyline(strokeColor, points);
             }
             else if (elementName == "line") {
-                    int x1 = child->IntAttribute("x1");
-                    int y1 = child->IntAttribute("y1");
-                    int x2 = child->IntAttribute("x2");
-                    int y2 = child->IntAttribute("y2");
-                    std::string stroke = child->Attribute("stroke");
-                    Color strokeColor = parse_color(stroke);
+                int x1 = child->IntAttribute("x1");
+                int y1 = child->IntAttribute("y1");
+                int x2 = child->IntAttribute("x2");
+                int y2 = child->IntAttribute("y2");
+                std::string stroke = child->Attribute("stroke");
+                Color strokeColor = parse_color(stroke);
 
-                    element = new Line(strokeColor, x1,y1,x2,y2);
+                element = new Line(strokeColor, x1,y1,x2,y2);
             }
             else if (elementName == "polygon") {
-                    std::string pointsStr = child->Attribute("points");
-                    std::vector<Point> points = parse_points(pointsStr);
-                    std::string fill = child->Attribute("fill");
-                    Color fillColor = parse_color(fill);
+                std::string pointsStr = child->Attribute("points");
+                std::vector<Point> points = parse_points(pointsStr);
+                std::string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
 
-                    element = new Polygon(fillColor, points);
+                element = new Polygon(fillColor, points);
             }
             else if (elementName == "rect") {
-                    int x = child->IntAttribute("x");
-                    int y = child->IntAttribute("y");
-                    int width = child->IntAttribute("width");
-                    int height = child->IntAttribute("height");
-                    std::string fill = child->Attribute("fill");
-                    Color fillColor = parse_color(fill);
+                int x = child->IntAttribute("x");
+                int y = child->IntAttribute("y");
+                int width = child->IntAttribute("width");
+                int height = child->IntAttribute("height");
+                std::string fill = child->Attribute("fill");
+                Color fillColor = parse_color(fill);
 
-                    element = new Rect(fillColor, x, y, width, height);
+                element = new Rect(fillColor, x, y, width, height);
             } 
             else if(elementName == "g") {
-                    std::vector<SVGElement *> elements;
-                    elements = parseGroup(child->FirstChildElement(), elements, id_pair);
-                    element = new Group(elements);
+                std::vector<SVGElement *> elements;
+                elements = parseGroup(child->FirstChildElement(), elements, id_pair);
+                element = new Group(elements);
             }
             else if(elementName == "use"){
-                    std::string href = child->Attribute("href");
-                    href = href.substr(1);
-                    for(auto& pair : id_pair){
-                        if (pair.first == href){
-                            element = pair.second->duplicate(href, pair.second);
-                        }
+                std::string href = child->Attribute("href");
+                href = href.substr(1);
+                for(auto& pair : id_pair)
+                {
+                    if (pair.first == href)
+                    {
+                        element = pair.second->duplicate(href, pair.second);
                     }
+                }
             }
             if(child == nullptr) return;
             
             //transf
             if (child->Attribute("transform")){
-                        //!@param tr Transformação do elemento
-                        string tr = child->Attribute("transform");
+                //!@param tr Transformação do elemento
+                string tr = child->Attribute("transform");
                         
-                        int ox = 0;
-                        int oy = 0; //default origin values
+                int ox = 0;
+                int oy = 0; //default origin values
 
-                        Point origin = Point{ox, oy};
+                Point origin = Point{ox, oy};
 
-                        if (child->Attribute("transform-origin")){
-                            string origin_str = child->Attribute("transform-origin");
-                            istringstream iss(origin_str);
-                            int x, y;
-                            iss >> x >> y;
+                if (child->Attribute("transform-origin")){
+                    string origin_str = child->Attribute("transform-origin");
+                    istringstream iss(origin_str);
+                    int x, y;
+                    iss >> x >> y;
 
-                            origin.x = x;
-                            origin.y = y;
-                        }
+                    origin.x = x;
+                    origin.y = y;
+                }
 
-                        if(tr.find("translate")!= string::npos){
+                if(tr.find("translate")!= string::npos){
 
-                            istringstream iss(tr);
+                    istringstream iss(tr);
 
-                            int x, y;
-                            char ch;
+                    int x, y;
+                    char ch;
 
-                            iss.ignore(tr.size(), '(');
-                            iss >> x;
-                            if (iss.peek() == ',') iss >> ch;
-                            iss  >> y;
+                    iss.ignore(tr.size(), '(');
+                    iss >> x;
+                    if (iss.peek() == ',') iss >> ch;
+                    iss  >> y;
 
-                            element->translate(x, y);
-                        }
-                        else if(tr.find("rotate") != string::npos){
-                            istringstream iss(tr);
+                    element->translate(x, y);
+                }
+                else if(tr.find("rotate") != string::npos){
+                    istringstream iss(tr);
 
-                            iss.ignore(tr.size(), '(');
-                            int angle;
-                            iss >> angle;
+                    iss.ignore(tr.size(), '(');
+                    int angle;
+                    iss >> angle;
 
-                            element->rotate(origin.x, origin.y, angle);
-                        }
-                        else if(tr.find("scale") != string::npos){
-                            istringstream iss(tr);
+                    element->rotate(origin.x, origin.y, angle);
+                }
+                else if(tr.find("scale") != string::npos){
+                    istringstream iss(tr);
 
-                            iss.ignore(tr.size(), '(');
-                            int factor;
-                            iss >> factor;
+                    iss.ignore(tr.size(), '(');
+                    int factor;
+                    iss >> factor;
 
-                            element->scale(origin.x, origin.y, factor);
-                        }
-                    }
+                    element->scale(origin.x, origin.y, factor);
+                }
+            }
 
             //push back
         if (child->Attribute("id")){
